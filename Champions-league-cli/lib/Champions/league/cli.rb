cc1e9684b8d073c8c970e2e14c2aaca6ecb99e04
Maurice Fiorani <mauricef97@gmail.com>
2020-03-13 02:48:43 +0000

    first commit

---
+++ Champions-league-cli/lib/Champions/league/cli.rb
@@ -0,0 +1,10 @@
+require "Champions/league/cli/version"
+
+module Champions
+  module League
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ Champions-league-cli/lib/Champions/league/cli/version.rb
@@ -0,0 +1,7 @@
+module Champions
+  module League
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
